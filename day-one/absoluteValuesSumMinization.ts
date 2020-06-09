12040adcc6296efe5ea2272bd008dbdcc12de0ff
hamza mostafa <ers.webservices@andalusiagroup.net>
2020-06-09 14:02:23 +0200

    Day1 first algo. solution

---
+++ day-one/absoluteValuesSumMinization.ts
@@ -0,0 +1,34 @@
+// solution 1 N(O^2)
+
+function absoluteValuesSumMinization1(array: number[]): number{
+    let winnerIndex: number[] = [];
+    for (let i = 0; i < array.length; i++) {
+        let totalSumsOfVariable = 0;
+        for (let j = 0; j < array.length; j++) {
+            totalSumsOfVariable += Math.abs(array[j] - array[i]);
+            if(i === 0){
+                winnerIndex[1] = totalSumsOfVariable;
+                winnerIndex[0] = 0;
+            }
+        }
+        if(totalSumsOfVariable < winnerIndex[1]){
+            winnerIndex[1] = totalSumsOfVariable;
+            winnerIndex[0] = i;
+        }
+        console.log(winnerIndex)
+    }
+    console.log(winnerIndex[1]);
+    return array[winnerIndex[0]];
+}
+
+// solution 2 N(O)
+
+function absoluteValuesSumMinization2(array: number[]): number{
+    // sort array
+    // middle number will always be the least
+    array.sort((a,b) => a-b);
+    console.log(Math.floor(array.length/2));
+    return array.length %2 ? array[Math.floor(array.length/2)] : array[Math.floor(array.length/2)-1];
+}
+
+console.log(absoluteValuesSumMinization1( [2, 4, 7, 6, 6, 8]));
