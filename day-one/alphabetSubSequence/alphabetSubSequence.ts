dbfc0d99c030a946617e5d00c53d99327c9388c3
hamza mostafa <ers.webservices@andalusiagroup.net>
2020-06-09 18:06:31 +0200

    Day1 sixth algo. solution second

--- day-one/alphabetSubSequence/alphabetSubSequence.ts
+++ day-one/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,10 +1,10 @@
 // solution O(N)
 
-function alphabetSubSequence(word: string) {
+function alphabetSubSequence(word: string): boolean {
 // convert to small case
     let status: number[] = [0];
     word = word.toLowerCase();
-    for (let i = 0; i+1 < word.length; i++) {
+    for (let i:number = 0; i+1 < word.length; i++) {
         word.charCodeAt(i) > word.charCodeAt(i+1) ? status.push(1): status.push(0);
     }
     return !status.reduce((a,b) => a+b);
---
+++ day-one/alphabeticShift/alphabeticShift.ts
@@ -0,0 +1,24 @@
+// first solution O(N), but with javascript split and join, and this causes another O(n)x2
+// so the total will be O(N)x3
+
+function alphabeticShift(word: string, shiftBy: number): string {
+    let wordCharArray: string[] = word.split('');
+    for (let i = 0; i < wordCharArray.length; i++) {
+        wordCharArray[i] =  String.fromCharCode(word.charCodeAt(i)+shiftBy);
+    }
+    return wordCharArray.join('');
+}
+
+console.log(alphabeticShift('cba', 4));
+
+// second solution without split and join, and saving the 2 steps
+
+function alphabeticShift2(word: string, shiftBy: number): string {
+    let newWord = '';
+    for (let i = 0; i < word.length; i++) {
+        newWord += String.fromCharCode(word.charCodeAt(i)+shiftBy);
+    }
+    return newWord;
+}
+
+console.log(alphabeticShift2('cba', 4));
